e26d0cce37973e0dda49f8aeeeafda73ac9bef0b
shouta0812 <shouta3012@ezweb.ne.jp>
2019-08-07 11:44:40 +0000

    cleanup

--- test/travis_package_make.bash
+++ test/travis_package_make.bash
@@ -1,6 +1,7 @@
 #!/bin/bash -xve
 
 #sync and make
-rsync -av ./ ~/catkin_ws/src/pimouse_ros/
+rsync -av ./ ~/catkin_ws/src/pimouse_run_corridor/
+git clone --depth=1 https://github.com/shouta0812/pimouse_ros.git
 cd ~/catkin_ws
 catkin_make
