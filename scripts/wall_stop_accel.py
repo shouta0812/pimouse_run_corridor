4b47502579aed14bb8434fb6a2017f8229db1c79
shouta0812 <shouta3012@ezweb.ne.jp>
2019-08-08 11:08:00 +0000

    cleanup

--- scripts/wall_stop_accel.py
+++ scripts/wall_stop_accel.py
@@ -18,20 +18,29 @@ class WallTrace():
         rate = rospy.Rate(10)
         data = Twist()
 
+        accel = 0.02
+        data.linear.x = 0.0
+        data.angular.z = 0
         while not rospy.is_shutdown():
-            data.linear.x = 0.2
-            data.angular.z = 0
-            if self.sensor_values.sum_all >= 500:
+            data.linear.x += accel
+
+            if self.sensor_values.sum_all > 50:
                 data.linear.x = 0
-                data.angular.z = 0
+            elif data.linear.x <= 0.2:
+                data.linear.x = 0.2
+            elif data.linear.x >= 0.8:
+                data.linear.x = 0.8
 
             self.cmd_vel.publish(data)
             rate.sleep()
 
 if __name__ == '__main__':
     rospy.init_node('wall_trace')
+
     rospy.wait_for_service('/motor_on')
     rospy.wait_for_service('/motor_off')
     rospy.on_shutdown(rospy.ServiceProxy('/motor_off',Trigger).call)
     rospy.ServiceProxy('/motor_on',Trigger).call()
-    WallTrace().run()
+
+    w = WallTrace()
+    w.run()
